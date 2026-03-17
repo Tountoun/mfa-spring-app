5bcef3d93c0110c1604c8eac48f9bf147ad01689
toun79 <tountounabela@gmail.com>
2026-03-17 22:52:42 +0000

    Start project by adding first dependencies and configurations

---
+++ src/test/java/com/gofar/mfa/TestApplication.java
@@ -0,0 +1,11 @@
+package com.gofar.mfa;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
